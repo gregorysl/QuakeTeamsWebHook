3555c6a5b8e7809b010d63e9c3feedfd99fc72e5
Grzegorz Slazak <gregorysl@gmail.com>
2018-10-17 21:49:20 +0200

    Parse connecting players info

--- QuakeTeamsWebHook/NewLog.cs
+++ QuakeTeamsWebHook/NewLog.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using NLog;
+using System.Text.RegularExpressions;
 
 namespace QuakeTeamsWebHook
 {
@@ -7,14 +8,19 @@ namespace QuakeTeamsWebHook
         private readonly Regex _actionsRegex;
         private readonly Regex _scorecardRegex;
         private readonly Regex _mapNameRegex;
+        private readonly Regex _userInfoChangedRegex;
+        private readonly Regex _clientConnectRegex;
 
         public Game Game { get; }
 
         public NewLog()
         {
-            _actionsRegex = new Regex($"({Consts.ShutdownGame}|{Consts.Score}|{Consts.Exit}|{Consts.InitGame})");
+
+            _actionsRegex = new Regex($"({Consts.ShutdownGame}|{Consts.Score}|{Consts.Exit}|{Consts.InitGame}|{Consts.ClientConnect}|{Consts.ClientUserinfoChanged})");
             _scorecardRegex = new Regex(@"score: (\d+)  ping: \d+  client: \d+ (.+)");
             _mapNameRegex = new Regex(@"mapname\\(.+?)\\");
+            _userInfoChangedRegex = new Regex(@"(\d+) n\\(.+?)\\t\\0\\");
+            _clientConnectRegex = new Regex($@"{Consts.ClientConnect} (\d+)");
             Game = new Game();
         }
 
@@ -35,18 +41,34 @@ namespace QuakeTeamsWebHook
                     var matches = _scorecardRegex.Match(line);
                     if (matches.Groups.Count == 3)
                     {
-                        var s = int.Parse(matches.Groups[1].Value);
-                        var card = new Scorecard(s, matches.Groups[2].Value);
+                        var s = int.Parse(GetMatchValue(matches, 1));
+                        var card = new Scorecard(s, GetMatchValue(matches, 2));
                         Game.Scorecard.Add(card);
                     }
+                    logger.Info("parsedScores");
                     break;
                 case Consts.InitGame:
-                    var nameMatch = _mapNameRegex.Match(line);
-                    Game.MapName = nameMatch.Groups[1].Value;
+                    var mapMatch = _mapNameRegex.Match(line);
+                    Game.MapName = GetMatchValue(mapMatch, 1);
+                    break;
+                case Consts.ClientConnect:
+                    var connectedMatch = _clientConnectRegex.Match(line);
+                    var connectedId = GetMatchValue(connectedMatch, 1);
+                    Game.AddPlayer(connectedId);
+                    break;
+                case Consts.ClientUserinfoChanged:
+                    var playerMatch = _userInfoChangedRegex.Match(line);
+                    var id = GetMatchValue(playerMatch, 1);
+                    var name = GetMatchValue(playerMatch, 2);
+                    Game.EditPlayer(id, name);
                     break;
                 default:
                     break;
             }
+        }
+        private string GetMatchValue(Match match, int i)
+        {
+            return match.Groups[i].Value;
         }
     }
 }
